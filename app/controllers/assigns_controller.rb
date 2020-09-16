ec10163d437795902ed4a60662f2a63a6475eae2
shotaro yamamoto <shotoron@gmail.com>
2020-09-16 23:13:49 +0900

    [add]チームリーダーか本人のみがチームからユーザーを削除できる機能を追加

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -2,6 +2,7 @@ class AssignsController < ApplicationController
   before_action :authenticate_user!
   before_action :email_exist?, only: [:create]
   before_action :user_exist?, only: [:create]
+  before_action :is_current_user_team_owner_or_target_user?, only: [:destroy]
 
   def create
     team = find_team(params[:team_id])
@@ -64,5 +65,12 @@ class AssignsController < ApplicationController
 
   def find_team(team_id)
     team = Team.friendly.find(params[:team_id])
+  end
+
+  def is_current_user_team_owner_or_target_user?
+    team = find_team(params[:team_id])
+    unless params[:id] == current_user.id || team.owner == current_user
+      redirect_to team_url(team), notice: '削除する権限がありません'
+    end
   end
 end
